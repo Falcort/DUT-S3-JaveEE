437baae51f39c6492bd35c95f6fd21fc3d01ffcc
Etienne Fonteix <etienne.fonteix@gmail.com>
2017-05-19 08:56:33 +0200

    Début TP3

--- src/java/MapremièreServlet.java
+++ src/java/MapremièreServlet.java
@@ -16,16 +16,19 @@ import javax.servlet.http.*;
  */
 @WebServlet(urlPatterns = {"/servlet"})
 public class MapremièreServlet extends HttpServlet {
-
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+    
+    public void init() {
+        
+    }
+    
+    public void service() {
+        
+    }
+    
+    public void destroy() {
+        
+    }
+    
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
